a27de59440d1383192eaa9b58aaff574ca1d0d40
scoutdiv <scoutdiv2358@gmail.com>
2024-11-08 21:15:11 +0530

    food commit

---
+++ Food Ordering App/FoodOrdering-main/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAqMgUid07sIeOvzXxEjV77U03-bAOUwYI",
+    authDomain: "foodweb-9dfe8.firebaseapp.com",
+    projectId: "foodweb-9dfe8",
+    storageBucket: "foodweb-9dfe8.firebasestorage.app",
+    messagingSenderId: "771646947915",
+    appId: "1:771646947915:web:e1757f45e5e356c8a083ce",
+    measurementId: "G-JE8DZ6BQ7Z"
+  }
+};
